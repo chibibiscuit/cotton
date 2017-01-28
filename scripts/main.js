8c7e81f48270be83a81c41417ca348e11786e719
chibibiscuit <chibibiscuit@gmail.com>
2017-01-27 18:02:59 -0600

    Corrected issue with API call

--- scripts/main.js
+++ scripts/main.js
@@ -46,9 +46,7 @@
 
         req.onreadystatechange = function () {
             if (req.readyState == XMLHttpRequest.DONE) {
-
-                //TODO - replace mockdata call once testing externally
-                data = req.response; //getMockData();
+                data = JSON.parse(req.response);
                 console.log(data);
 
                 initImageHash();
@@ -178,138 +176,6 @@
         setTimeout(function(){
             toastElement.style.opacity = 0;
         }, toastDuration)
-    }
-
-    
-
-    function getMockData(){
-        return {
-        "photoset": {
-            "id": "72157626579923453",
-            "primary": "5713357208",
-            "owner": "30966612@N02",
-            "ownername": "dottie gale",
-            "photo": [
-            {
-                "id": "5713357208",
-                "secret": "a164f169c6",
-                "server": "3492",
-                "farm": 4,
-                "title": "49320001",
-                "isprimary": "1",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5713357308",
-                "secret": "62e7bfd8f1",
-                "server": "2364",
-                "farm": 3,
-                "title": "49320002",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5712796377",
-                "secret": "27deb48f69",
-                "server": "3622",
-                "farm": 4,
-                "title": "49320005",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5712796471",
-                "secret": "93c26331f4",
-                "server": "2215",
-                "farm": 3,
-                "title": "49320006",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5712796591",
-                "secret": "e54e4f24e2",
-                "server": "2723",
-                "farm": 3,
-                "title": "49320007",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5712796761",
-                "secret": "1d5675bcca",
-                "server": "2026",
-                "farm": 3,
-                "title": "49320009",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5713357952",
-                "secret": "940cf2fb26",
-                "server": "2194",
-                "farm": 3,
-                "title": "49320010",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5713358054",
-                "secret": "14f1a2434c",
-                "server": "2302",
-                "farm": 3,
-                "title": "49320014",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5713358170",
-                "secret": "d010fb1302",
-                "server": "3640",
-                "farm": 4,
-                "title": "49320011",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            },
-            {
-                "id": "5713358278",
-                "secret": "69f0929361",
-                "server": "3013",
-                "farm": 4,
-                "title": "49320013",
-                "isprimary": "0",
-                "ispublic": 1,
-                "isfriend": 0,
-                "isfamily": 0
-            }
-            ],
-            "page": 1,
-            "per_page": "10",
-            "perpage": "10",
-            "pages": 5,
-            "total": "47",
-            "title": "Thailand Trip - 2010"
-        },
-        "stat": "ok"
-        }
     }
 
     
